599951af41425b9ab11cfeee58a24e0746f16f8d
BEST99FRIEND <asadovahror9@gmail.com>
2025-11-14 09:46:41 +0500

    test html

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
-from main.views import hello
+from main.views import hello, books_list, authors_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', hello),
+    path('books/', books_list, name='books_list'),
+    path('authors/', authors_list, name='authors_list'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, Author
 
 def hello(request):
     return HttpResponse("Hello, World!")
+
+
+def books_list(request):
+    books = Book.objects.select_related('author').all().order_by('-published_date')
+    return render(request, 'main/books_list.html', {'books': books})
+
+
+def authors_list(request):
+    authors = Author.objects.all().order_by('name')
+    return render(request, 'main/authors_list.html', {'authors': authors})
